fe235344090b92073a5945597ac185029cf710df
meszarostibor <71766087+mszarostibor@users.noreply.github.com>
2021-11-25 09:34:50 +0100

    órai munka

--- 20211125_tibi.py
+++ 20211125_tibi.py
@@ -4,10 +4,11 @@ def feladat1():
     szamok=[]
     for i in range(20, 41):
         szamok.append(i**2)
+    print(szamok)
 
 def feladat2():
     szoveg="Az ősz annyira szereti a naplementéket, hogy minden egyes levélre egyet fest."
-    print(len(szoveg))
+    print('karakterek száma',len(szoveg))
     darab = 0
     szavak = szoveg.split()
     print('hogy hány szóból áll', szavak)
@@ -16,10 +17,11 @@ def feladat2():
             darab += 1
         return darab
     szam=[]
+    print('darabszáma', darab)
     magánhanzok=['aáeéiíoóöőuúüű']
     if magánhanzok in szoveg:
         szam.append(1)
-    print(szam)
+    print('magánhangzok száma', szam)
 
 def feladat3():
     szamok=[]
@@ -31,8 +33,17 @@ def feladat3():
     for szam in szamok:
         osszeg += szam
         atlag += szam / len(szamok)
+    print('összege',osszeg)
+    print('atlaga',atlag)
     legnagyobb=max(szamok)
     legkisebb=min(szamok)
+    terjedelme = legnagyobb - legkisebb
+    print('terjedelme',terjedelme)
+    for item in szamok:
+        if item %3 == 0:
+            oszthatok.append(item)
+    print('Ezek a számok osszthatok hárommal', oszthatok)
+
     terjedelme = legnagyobb - legkisebb
     for item in szamok:
         if item %3 == 0:
