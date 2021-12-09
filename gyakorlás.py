8685c568e2dfe5612110046cf9a6bbf250acd45b
meszarostibor <71766087+mszarostibor@users.noreply.github.com>
2021-12-09 09:40:20 +0100

    orai munka

---
+++ gyakorlás.py
@@ -0,0 +1,118 @@
+from math import *
+import random
+def feladat1():
+    a = str("nbfoahbfoainf")
+def feladat2():
+    b="a"
+def feladat3():
+    c=21
+def feladat4():
+    n=2.5
+def feladat5():
+    d=True
+def feladat6():
+    e=input("irj egy szot ")
+def feladat7():
+    f=input("irj egy mondatot ")
+def feladat8():
+    g=input("irj egy karaktert ")
+def feladat9():
+    h=int(input("irj egy egész számot "))
+def feladat10():
+    i=float(input("irj egy valos számot "))
+    print(i*2)
+    print(i*math.pi)
+    print(i**3)
+    print(sqrt(i))
+    print(round(i//2,2))
+def feladat11():
+    j=random.randint(10,50)
+def feladat12():
+    k=int(input("irj egy egész számot "))
+    if k%2==0:
+        print("páros a szám")
+    else:
+        print("páratlan a szám")
+def feladat13():
+    l=int(input("irj egy egész számot "))
+    if l>0:
+        print("pozitiv a szám")
+    elif l<0:
+        print("negativ a szám")
+    else:
+        print("a szám nulla")
+def feladat14():
+    m=int(input("irj egy egész számot "))
+    if m>30:
+        print("nagyobb mint 30")
+    else:
+        print("nem nagyobb mint 30")
+def feladat15():
+    aa=int(input("irj egy egész számot "))
+    ab=int(input("irj egy egész számot "))
+    if aa>ab:
+        print(aa,"a nagyobb")
+    else:
+        print(ab,"a nagyobb")
+def feladat16():
+    ba=int(input("irj egy egész számot "))
+    bb=int(input("irj egy egész számot "))
+    if ba<bb:
+        print(ba,"a kisebb")
+    else:
+        print(bb,"a kisebb")
+def feladat17():
+    o=input("irj egy szot ")
+    print(len(o))
+def feladat18():
+    u=input("irj egy szot ")
+    uu=u.count("e")
+    print(uu)
+def feladat19():
+    p=input("irj egy szot ")
+    pp=p.count("a","á","e","é","i","í","o","ó","ö","ő","u","ú","ü","ű")
+    print(pp,"magánhangzó van")
+def feladat20():
+    q=("irj egy mondatot ")
+    print(len(q))
+def feladat21():
+    w=("irj egy mondatot ")
+    ww = w.count(" ")
+    print(ww)
+def feladat22():
+    r=("irj egy mondatot ")
+    rr=r.count("e")
+    print(rr)
+def feladat23():
+    t=("irj egy mondatot ")
+    tt=t.count(" ")+1
+    print(tt)
+def feladat24():
+    z=("irj egy mondatot ")
+    zz=z.split()
+    print("a szavak hossza",len(zz))
+def feladat25():
+    s=("irj egy mondatot ")
+    sa=s.split(' ')
+    
+feladat1()
+feladat2()
+feladat3()
+feladat4()
+feladat5()
+feladat6()
+feladat7()
+feladat8()
+feladat9()
+feladat10()
+feladat11()
+feladat12()
+feladat13()
+feladat14()
+feladat15()
+feladat16()
+feladat17()
+feladat18()
+feladat19()
+feladat20()
+feladat21()
